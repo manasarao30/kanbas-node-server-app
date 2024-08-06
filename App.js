f064695f1cdba58d2fe7f583253ca7839bc09f32
manasarao54747@gmail.com <manasarao54747@gmail.com>
2024-08-05 22:29:22 -0400

    changes

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-
 import cors from "cors";
 const app = express();
 app.use(cors());                    // make sure cors is used right after creating the app
@@ -12,7 +11,7 @@ app.use(express.json());
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
-
+Hello(app);
 Lab5(app);                          // express instance
 app.listen(process.env.PORT || 4000)
 
