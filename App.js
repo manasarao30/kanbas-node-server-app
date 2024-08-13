a643278240fa143263d72fde59a5a8d1cc48be4e
manasarao54747@gmail.com <manasarao54747@gmail.com>
2024-08-12 21:19:02 -0400

    a6 changes

--- App.js
+++ App.js
@@ -18,35 +18,26 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000"
+    origin: "https://a6--kanbas-react-web-app-su2.netlify.app" || "http://localhost:3000"
     // origin: true
   })
 );
-// const sessionOptions = {
-//   secret: process.env.SESSION_SECRET || "kanbas",
-//   resave: false,
-//   saveUninitialized: false,
-// };
-// if (process.env.NODE_ENV !== "development") {
-//   sessionOptions.proxy = true;
-//   sessionOptions.cookie = {
-//     sameSite: "none",
-//     secure: true,
-//     domain: process.env.NODE_SERVER_DOMAIN,
-//   };
-// }
-// app.use(session(sessionOptions));
-
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    sameSite: "None",
-    secure: true,
-  },
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
 app.use(session(sessionOptions));
+
+
 
 app.use(express.json());
 UserRoutes(app);
